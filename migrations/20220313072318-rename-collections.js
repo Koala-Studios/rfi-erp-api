cc0a4e991cdd93b3d90d07966405364778c827d6
frankev6 <frank.ev6@gmail.com>
2022-03-16 00:21:21 -0400

    Product Controller and Model Changes

--- migrations/20220313072318-rename-collections.js
+++ migrations/20220313072318-rename-collections.js
@@ -1,12 +1,15 @@
 module.exports = {
   async up(db, client) {
     // TODO write your migration here.
-
-    await db.collection("batchings").drop();
-    await db.renameCollection("Batching", "batchings");
+    // await db.collection("products").drop();
+    // await db.collection("batchings").drop();
+    // await db.renameCollection("Batching", "batchings");
+    // await db.collection("Product").copyTo("products");
+    // await db.Batching.copyTo("batchings");
   },
 
   async down(db, client) {
-    await db.renameCollection("batchings", "Batching");
+    // await db.renameCollection("batchings", "Batching");
+    // await db.renameCollection("products", "Product");
   },
 };
