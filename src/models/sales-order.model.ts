f87f3880666172547ea0095a75459c61201e04f5
danveloso <danielvelo11@gmail.com>
2022-12-01 11:51:45 -0500

    filled sales order model

---
+++ src/models/sales-order.model.ts
@@ -0,0 +1,45 @@
+import mongoose from "mongoose";
+
+interface IOrderItem {
+  product_code: string;
+  customer_p_code:string;
+  amount: number;
+  price: number;
+  status: number;
+  product_id: string;
+  product_name:string;
+  lot_number:string;
+}
+
+export interface ISalesOrder extends mongoose.Document {
+    date_of_purchase: Date;
+    date_sent?: Date;
+    order_code: string;
+    customer_id: string;
+    customer_name: string;
+    notes: string;
+    status: number;
+    order_items: [IOrderItem];
+}
+
+const salesOrderSchema = new mongoose.Schema({
+    date_purchased: Date,
+    date_arrived: Date,
+    customer_id: String,
+    customer_name: String,
+    order_code: String,
+    status: Number,
+    order_items: 
+    [{
+        product_code: String,
+        customer_p_code:String,
+        amount: Number,
+        price: Number,
+        status: Number,
+        product_id: String,
+        product_name: String,
+        lot_number:String,
+    }],
+});
+
+export default mongoose.model<ISalesOrder>("Sales Order", salesOrderSchema, 'sales');
