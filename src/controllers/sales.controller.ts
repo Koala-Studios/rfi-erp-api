4111bc6966725c9270a2a659b50d0cb3e7fa191e
danveloso <danielvelo11@gmail.com>
2022-12-06 22:03:20 -0500

    sales controller & logic

--- src/controllers/sales.controller.ts
+++ src/controllers/sales.controller.ts
@@ -1,1 +1,55 @@
 //TODO: Pagination and.. the whole page.
+
+
+import { Security } from "@tsoa/runtime";
+import { Body } from "@tsoa/runtime";
+import { SuccessResponse } from "@tsoa/runtime";
+import { Controller } from "@tsoa/runtime";
+import { Post } from "@tsoa/runtime";
+import { Tags } from "@tsoa/runtime";
+import { Get } from "@tsoa/runtime";
+import { Query } from "@tsoa/runtime";
+import { Put } from "@tsoa/runtime";
+import { Route } from "@tsoa/runtime";
+import { Request } from "@tsoa/runtime";
+import { Request as eRequest, Response } from "express";
+import logger from "../logger/logger";
+import { reply, status } from "../config/config.status";
+import { getSalesOrder, listSalesOrders } from "../logic/sales.logic";
+
+@Route("sales-orders")
+@Tags("SalesOrder")
+@Security("jwt")
+export class SalesController extends Controller {
+    @Get("list")
+    @SuccessResponse(status.OK, reply.success)
+    public async listSalesRequest(
+    @Request() req: eRequest,
+    @Query() page: string,
+    @Query() count: string
+  ) {
+      const _page = parseInt(<string>page);
+      const _count = parseInt(<string>count);
+      const res = await listSalesOrders({
+        page: _page,
+        count: _count,
+        filter: "" });
+      this.setStatus(res.status);
+      return res.data;
+    }
+
+
+      
+    @Get("get")
+    @SuccessResponse(status.OK, reply.success)
+    public async getFormulaRequest(
+      @Request() req: eRequest,
+      @Query() id: string,
+    ) {
+      const _res = await getSalesOrder(id);
+      
+      this.setStatus(_res.status);
+      console.log(_res)
+      return _res.data;
+    }
+}
--- src/logic/sales.logic.ts
+++ src/logic/sales.logic.ts
@@ -1,1 +1,131 @@
-//TODO: Pagination and .. the whole page
+import { reply, status } from "../config/config.status";
+import SalesOrder, { ISalesOrder } from "../models/sales-order.model";
+import Inventory from "../models/inventory.model";
+import mongoose from "mongoose";
+import { IListParams, IListResponse, ILogicResponse } from "./interfaces.logic";
+let ObjectId = require('mongodb').ObjectId;
+
+
+export const listSalesOrders = async (
+    listParams: IListParams
+  ): Promise<ILogicResponse> => {
+    const list = await SalesOrder.paginate(
+      {},
+      { page: listParams.page, limit: listParams.count, leanWithId: true }
+    );
+  
+    return {
+      status: status.OK,
+      data: { message: "", res: list },
+    };
+  };
+
+
+export const getSalesOrder = async (po_id):Promise<ILogicResponse> => {
+
+    let _status:number;
+
+    const p_id = new ObjectId( po_id)
+
+   const _sales_order = await SalesOrder.aggregate(
+    [
+        {
+            "$match" :{
+                "_id" : p_id ,
+            },
+
+        },
+        {
+            "$lookup" : {
+                "from" : "inventory",
+                "let" : {
+                    "ingredients" : "$order_items"
+                },
+                "pipeline" : [
+                    {
+                        "$match" : {
+                            "$expr" : {
+                                "$in" : [
+                                    "$_id",
+                                    "$$ingredients.material_id"
+                                ]
+                            }
+                        }
+                    },
+                    {
+                        "$project" : {
+                            "name" : 1.0
+                        }
+                    }
+                ],
+                "as" : "order_info"
+            }
+        }, 
+        {
+            "$addFields" : {
+                "order_items" : {
+                    "$map" : {
+                        "input" : "$order_items",
+                        "in" : {
+                            "$let" : {
+                                "vars" : {
+                                    "m" : {
+                                        "$arrayElemAt" : [
+                                            {
+                                                "$filter" : {
+                                                    "input" : "$order_info",
+                                                    "cond" : {
+                                                        "$eq" : [
+                                                            "$$mb._id",
+                                                            "$$this.material_id"
+                                                        ]
+                                                    },
+                                                    "as" : "mb"
+                                                }
+                                            },
+                                            0.0
+                                        ]
+                                    }
+                                },
+                                "in" : {
+                                    "$mergeObjects" : [
+                                        "$$this",
+                                        {
+                                            "material_name" : "$$m.name"
+                                        }
+                                    ]
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        },
+        {
+            "$unset" : [
+                "order_info"
+            ]
+        }
+    ], 
+    {
+        "allowDiskUse" : false
+    }
+);
+    if(!_sales_order[0]) {
+        _status = status.OK;
+      return {status:_status, data:{message:"No Sales Order Found",res:null}};
+    }
+    _status = status.OK;
+
+    return {status:_status, data:{message:"Sales Order",res:_sales_order[0]}};
+}
+
+
+export const updateSalesItem = async (placeholder):Promise<ILogicResponse> => {
+
+    //remember there's 
+    //send to QC or receive into inventory
+
+    return {status: null, data:{message: null,res: null}};
+}
+
